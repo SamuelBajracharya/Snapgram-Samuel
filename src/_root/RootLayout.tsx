ad3fe89fba950c91ef696da9f1cebdfb11191f7b
Samuel Bajracharya <linuxsmuffproductivity@gmail.com>
2024-10-26 20:13:47 +0545

    first commit

---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RootLayout = () => {
+  return (
+    <div className='underline'>RootLayout</div>
+  )
+}
+
+export default RootLayout
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; 
+import { BrowserRouter } from "react-router-dom";
+
+const rootElement = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(rootElement).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
